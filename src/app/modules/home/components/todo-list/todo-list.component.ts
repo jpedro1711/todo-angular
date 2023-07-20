51c0406bdca3f3c4e096fac4082cc5f8bd22e0d1
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2023-07-20 10:50:28 -0300

    delete one and delete all

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova task', checked: true },
+    { task: 'Minha nova task 2', checked: false },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public deleteItemTaskList(index: number) {
+    this.taskList.splice(index, 1);
+  }
+
+  public deleteAllTaskList() {
+    const c = confirm('Você realmente deseja deletar tudo? ');
+    if (c) {
+      this.taskList = [];
+    }
+  }
 }
