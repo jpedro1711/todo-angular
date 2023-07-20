37aa6028343bdaa22aa4442c99d777d756637a6c
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2023-07-20 11:23:41 -0300

    handle of todo check and sort todos

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
   public taskList: Array<TaskList> = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngDoCheck(): void {
+    this.taskList.sort(
+      (first, last) => Number(first.checked) - Number(last.checked)
+    );
+  }
 
   public deleteItemTaskList(index: number) {
     this.taskList.splice(index, 1);
@@ -22,10 +26,23 @@ export class TodoListComponent implements OnInit {
     }
   }
 
+  public toogleChecked(index: number) {
+    this.taskList[index].checked == !this.taskList[index].checked;
+  }
+
   public deleteAllTaskList() {
     const c = confirm('Você realmente deseja deletar tudo? ');
     if (c) {
       this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number) {
+    if (!event.length) {
+      const c = confirm('Você deseja excluir essa task? ela está vazia');
+      if (c) {
+        this.deleteItemTaskList(index);
+      }
     }
   }
 }
